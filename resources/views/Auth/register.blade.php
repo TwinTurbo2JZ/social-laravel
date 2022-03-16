8bd8ba4660de0962e2d78da6f976402b4a251745
DESKTOP-S04RBAC\Pron <pronojit@protonmail.ch>
2022-03-16 16:42:39 +0530

    adding basic register view and route

---
+++ resources/views/Auth/register.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.app')
+
+@section('content')
+
+<div class="flex justify-center">
+    <div class="w-5/12 bg-white p-6 rounded-lg">
+        register users here
+    </div>
+</div>
+
+@endsection
