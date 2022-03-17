b01833509eda5804cd5ee4c610655cb6c6bbe369
DESKTOP-S04RBAC\Pron <pronojit@protonmail.ch>
2022-03-17 19:59:56 +0530

    adding remember me checkbox on login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
             'password' => 'required'
         ]);
    
-        if(!auth()->attempt($request->only('email', 'password'))) {
+        if(!auth()->attempt($request->only('email', 'password'), $request-> remember)) {
             return back()->with('status', 'invalid Login details');
         }
 
--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -31,7 +31,12 @@
                     @enderror
                 </div>
 
-                
+                <div class="mb-3">
+                    <div class="flex flex-center form-check">
+                        <input type="checkbox" name="remember" id="remember" class="form-check-input h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer">
+                        <label for="remember" class="form-check-label inline-block text-gray-800">Remember me</label>
+                    </div>
+                </div>
 
                 <div>
                     <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Login</button>
